dbe027a5021b3d30fcfeb7d594948f037ddd45b4
Panos "AlwaysLivid" V <panos@livid.pw>
2018-06-30 17:59:43 +0300

    Polished the code a little bit. *I'm a perfectionist*

--- src/main.py
+++ src/main.py
@@ -30,19 +30,18 @@ bot = commands.Bot(command_prefix=config.prefix, description=config.description)
 async def CogLoader(): # Loads cogs.
     for file in config.extensions: # file = list item in config.extensions
         try:
-            logging.info("Loading %s...", file)
+            logging.info("Loading {}...".format(file))
             bot.load_extension(file)
-            logging.info("Loaded %s...", file)
+            logging.info("Loaded {}...".format(file))
         except Exception as e:
-            logging.critical("FAILED TO LOAD %s", file)
-            logging.critical("Exception: %s", e)
-            logging.critical(traceback.print_exc())
+            logging.critical("FAILED TO LOAD {}".format(file))
+            logging.critical("Exception: {}".format(e))
 
 @bot.event
 async def on_ready():
     logging.info("Ready!")
-    logging.info("Signed in as %s (ID: %s)", bot.user.name, bot.user.id)
-    logging.info("Discord Version: %s", discord.__version__)
+    logging.info("Signed in as {} (ID: {})".format(bot.user.name, bot.user.id))
+    logging.info("Discord Version: {}".format(discord.__version__))
     await CogLoader()
     if config.debug_mode == True:
         activity = config.debug_status
